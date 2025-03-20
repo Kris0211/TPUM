f7f10be5b129f7e6a3295a5bbcd62d5fe830ba5f
Kris <stawinogakris@gmail.com>
2025-03-20 23:27:20 +0100

    Add example program and tests

---
+++ ExampleProgram/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace TPUM
+{
+    public class Class1
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Sub(int x, int y) 
+        {
+            return x - y; 
+        }
+    }
+}
---
+++ ExampleTest/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace ExampleTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TPUM.Class1 example = new TPUM.Class1();
+            int sum = example.Add(2, 2);
+            Assert.AreEqual(sum, 4);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            TPUM.Class1 example = new TPUM.Class1();
+            int diff = example.Sub(5, 3);
+            Assert.AreEqual(2, diff);
+        }
+    }
+}
