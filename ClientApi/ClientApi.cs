360f6789c67aeb31967f9dd1ea19bf4c7c0caf9e
Filip <93220072+toLLto@users.noreply.github.com>
2025-04-04 09:19:52 +0200

    feat: ClientApi

---
+++ ClientApi/ClientApi.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace ClientApi
+{
+    [Serializable]
+    public abstract class ServerCommand
+    {
+        public string Header;
+
+        protected ServerCommand(string header) 
+        {
+            Header = header;
+        }
+    }
+
+    [Serializable]
+    public class  GetItemsCommand : ServerCommand
+    {
+        public static string HeaderStatic = "GetItems";
+
+        public GetItemsCommand() : base(HeaderStatic) { }
+    }
+
+    [Serializable]
+    public class SellItemCommand : ServerCommand
+    {
+        public static string HeaderStatic = "SellItem";
+
+        public Guid TransactionId;
+        public Guid ItemId;
+
+        public SellItemCommand(Guid id) : base(HeaderStatic)
+        {
+            TransactionId = Guid.NewGuid();
+            ItemId = id;
+        }
+    }
+
+    [Serializable]
+    public struct ItemDTO
+    {
+        public Guid Id;
+        public string Name;
+        public string Description;
+        public string Type;
+        public float Price;
+        public bool IsSold;
+
+        public ItemDTO(Guid id, string name, string description, string type, float price, bool isSold)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Type = type;
+            Price = price;
+            IsSold = isSold;
+        }
+    }
+
+    [Serializable]
+    public struct NewPriceDTO
+    {
+        public Guid ItemId;
+        public float NewPrice;
+
+        public NewPriceDTO(Guid id, float newPrice)
+        {
+            ItemId = id;
+            NewPrice = newPrice;
+        }
+    }
+
+    [Serializable]
+    public abstract class ServerResponse
+    {
+        public string Header { get; private set; }
+
+        protected ServerResponse(string header)
+        {
+            Header = header;
+        }
+    }
+
+    [Serializable]
+    public class UpdateAllResponse : ServerResponse
+    {
+        public static readonly string HeaderStatic = "UpdateAllItems";
+
+        public ItemDTO[]? Items;
+
+        public UpdateAllResponse() : base(HeaderStatic) { }
+    }
+
+    [Serializable]
+    public class ReputationChangedResponse : ServerResponse
+    {
+        public static readonly string HeaderStatic = "ReputationChanged";
+
+        public float NewReputation;
+        public NewPriceDTO[]? NewPrices;
+
+        public ReputationChangedResponse() : base(HeaderStatic) { }
+    }
+
+    [Serializable]
+    public class TransactionResponse : ServerResponse
+    {
+        public static readonly string HeaderStatic = "TransactionResponse";
+
+        public Guid TransactionId;
+        public bool Succeeded;
+
+        public TransactionResponse() : base(HeaderStatic) { }
+}
---
+++ ClientApi/WebSocketConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClientApi
+{
+    public abstract class WebSocketConnection
+    {
+        public virtual Action<string>? OnMessage { protected get; set; } = x => { };
+        public virtual Action? OnClose { protected get; set; } = () => { };
+        public virtual Action? OnError { protected get; set; } = () => { };
+
+        public async Task SendAsync(string message)
+        {
+            await SendTask(message);
+        }
+
+        public abstract Task DisconnectAsync();
+
+        protected abstract Task SendTask(string message);
+    }
+}
